33e706f24ba16f8f0b5b889fb10ee299ab290b6d
Sitoh7 <ha.wakhule@gmail.com>
2024-11-26 13:23:59 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    my_list=[0,1]
     pass
+    if(length==0):
+        my_list=[]
+    elif(length==1):
+        my_list=[0]
+    else:
+        for i in range(length-2):
+           my_list.append(my_list[i+1]+my_list[i]) 
+    
+    print(my_list)
+        
+            
